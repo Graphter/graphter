d91fdb3c232813907589836641be72a9367293c8
Jonathan Pritchard <jon@node28.com>
2020-10-24 11:32:32 +0100

    Added intro graphic to generator

--- packages/graphter/index.js
+++ packages/graphter/index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
 const execSync = require('child_process').execSync;
+const figlet = require('figlet')
 
-execSync('npx create-react-app --template cra-template-graphter-typescript ' + process.argv.slice(2).join(' '))
+console.log(figlet.textSync('Graphter', {
+  font: 'Electronic'
+}))
+console.log('Generating your new project...')
+execSync('npx create-react-app --template cra-template-graphter-typescript ' + process.argv.slice(2).join(' '), { stdio: 'inherit' })
