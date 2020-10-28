f20ab4080fe88ef70af54f0218bfd808778b3902
Jonathan Pritchard <jon@node28.com>
2020-10-28 08:46:41 +0000

    Remove scaffolding work and replace with placeholder

--- packages/graphter/index.js
+++ packages/graphter/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-const execSync = require('child_process').execSync;
 const figlet = require('figlet')
 
 console.log(figlet.textSync('Graphter', {
   font: 'Electronic'
 }))
-console.log('Generating your new project...')
-execSync('npx create-react-app --template cra-template-graphter-typescript ' + process.argv.slice(2).join(' '), { stdio: 'inherit' })
+console.log('The project scaffolder will live here when we make it.')
+console.log()
+console.log('For now refer to the basic example.')
+console.log()
+console.log()
