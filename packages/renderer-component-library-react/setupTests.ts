aa61a3952df969313de3e1efdb3c02d7f7d735a8
Jonathan Pritchard <jon@node28.com>
2020-11-29 16:00:48 +0000

    Add testing lib jest-dom extend-expect to test setup

--- packages/renderer-component-library-react/setupTests.ts
+++ packages/renderer-component-library-react/setupTests.ts
@@ -2,6 +2,8 @@ import * as Enzyme from 'enzyme'
 // @ts-ignore
 import ReactSixteenAdapter from 'enzyme-adapter-react-16'
 
+import '@testing-library/jest-dom/extend-expect';
+
 Enzyme.configure({
   adapter: new ReactSixteenAdapter(),
 });
