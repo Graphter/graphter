b6c2b916d558b52dbf9477e738452fb33c7bf5ee
Jonathan Pritchard <jon@node28.com>
2020-11-29 16:10:51 +0000

    Made options.... optional

--- packages/renderer-component-library-react/src/node-renderers/string/registerStringNodeRenderer.ts
+++ packages/renderer-component-library-react/src/node-renderers/string/registerStringNodeRenderer.ts
@@ -6,7 +6,7 @@ export interface StringNodeRendererOptions {
   type: string
 }
 
-export function registerStringNodeRenderer(options: StringNodeRendererOptions): NodeRendererRegistration {
+export function registerStringNodeRenderer(options?: StringNodeRendererOptions): NodeRendererRegistration {
   return {
     type: options?.type || 'string',
     jsonType: JsonType.STRING,
