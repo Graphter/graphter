ea4517ffe6e4aa2cb7d06026be7b2a601090de0f
Jonathan Pritchard <jon@node28.com>
2020-11-29 16:01:11 +0000

    mock out renderer-react dependency

---
+++ packages/renderer-component-library-react/__mocks__/@graphter/renderer-react.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { JsonType } from "@graphter/core";
+
+export const useArrayNodeData = jest.fn(() => {
+  return {
+    childIds: [],
+    removeItem: jest.fn(),
+    commitItem: jest.fn()
+  }
+})
+export const nodeRendererStore = {
+  get: jest.fn(() => ({
+    type: 'string',
+    jsonType: JsonType.STRING,
+    renderer: ({originalNodeData}: any) => {
+      const [ value, setValue ] = useState(originalNodeData || '')
+      return (
+        <input value={value} onChange={e => setValue(e.target.value)} data-testid='string-renderer' />
+    )
+    }
+  }))
+}
+export const createDefault = jest.fn()
