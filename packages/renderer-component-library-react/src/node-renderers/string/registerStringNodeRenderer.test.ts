02d0bda87ae3ef70e47ae99564aa0f55390c291d
Jonathan Pritchard <jon@node28.com>
2020-12-29 18:17:22 +0000

    Added test coverage for string node renderer registration

---
+++ packages/renderer-component-library-react/src/node-renderers/string/registerStringNodeRenderer.test.ts
@@ -0,0 +1,16 @@
+import StringNodeRenderer, { registerStringNodeRenderer } from "../string";
+
+describe('registerStringNodeRenderer()', () => {
+  it('should return the correct registration', async () => {
+    const result = registerStringNodeRenderer()
+    expect(result.type).toBe('string')
+    expect(result.getChildData).toBeUndefined()
+    expect(result.getChildPaths).toBeUndefined()
+    expect(result.renderer).toBe(StringNodeRenderer)
+    expect(result.options).toBeUndefined()
+  })
+  it('should override the type if supplied in options', async () => {
+    const result = registerStringNodeRenderer({ type: 'custom-string' })
+    expect(result.type).toBe('custom-string')
+  })
+})
