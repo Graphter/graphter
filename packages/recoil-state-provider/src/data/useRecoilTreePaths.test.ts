a9862d90ff5b527802cd6bb37b22376a5ed646fd
Jonathan Pritchard <jon@node28.com>
2020-12-29 19:45:48 +0000

    Fixed and added tests for useRecoilTreePaths hook

--- packages/recoil-state-provider/src/data/useRecoilTreePaths.test.ts
+++ packages/recoil-state-provider/src/data/useRecoilTreePaths.test.ts
@@ -1,4 +1,4 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilValueLoadable } from "recoil";
 import { useRecoilTreePaths } from './useRecoilTreePaths'
 import { when } from "jest-when";
 import treeDataStore from "../store/treeDataStore";
@@ -6,7 +6,7 @@ import treeDataStore from "../store/treeDataStore";
 jest.mock('recoil')
 jest.mock('../store/treeDataStore')
 
-const useRecoilValueMock = useRecoilValue as jest.Mock<any>
+const useRecoilValueLoadableMock = useRecoilValueLoadable as jest.Mock<any>
 const treeDataStoreMock = treeDataStore as jest.Mocked<any>
 
 describe('useRecoilTreePaths', () => {
@@ -17,12 +17,29 @@ describe('useRecoilTreePaths', () => {
     when(treeDataStoreMock.getDescendentPaths)
       .calledWith(['0'])
       .mockReturnValueOnce({ some: 'state' })
-    when(useRecoilValueMock)
+    when(useRecoilValueLoadableMock)
       .calledWith({ some: 'state' })
-      .mockReturnValue([['title'], ['name']])
+      .mockReturnValue({
+        state: 'hasValue',
+        contents: [ [ 'title' ], [ 'name' ] ]
+      })
     const result = useRecoilTreePaths(['0'])
     expect(result).toEqual([['title'], ['name']])
   })
+  it.each(['loading', 'hasError' ])
+    ('should return no paths while in "%s" state', (state) => {
+      when(treeDataStoreMock.getDescendentPaths)
+        .calledWith(['0'])
+        .mockReturnValueOnce({ some: 'state' })
+      when(useRecoilValueLoadableMock)
+        .calledWith({ some: 'state' })
+        .mockReturnValue({
+          state,
+          contents: [ [ 'title' ], [ 'name' ] ]
+        })
+      const result = useRecoilTreePaths(['0'])
+      expect(result).toEqual([])
+    })
 })
 
 export {}
