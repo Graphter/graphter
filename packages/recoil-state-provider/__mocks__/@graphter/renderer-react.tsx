e134dc929e2c180b623b41fce1ba60bf94bf87fa
Jonathan Pritchard <jon@node28.com>
2021-01-01 18:42:46 +0000

    Mock out @graphter/renderer-react

---
+++ packages/recoil-state-provider/__mocks__/@graphter/renderer-react.tsx
@@ -0,0 +1,10 @@
+import { NodeRendererRegistration } from "@graphter/core";
+
+export const nodeRendererStore = {
+  get: jest.fn()
+}
+
+export const pathConfigStore = {
+  get: jest.fn(),
+  set: jest.fn()
+}
