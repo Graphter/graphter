2394ea708d271b89417cc9b163900a261903028c
Jonathan Pritchard <jon@node28.com>
2021-01-13 09:51:28 +0000

    Removed the main site project as we'll come back to that later

--- site/main/src/components/Demo/ManageConfig.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ManageConfig(){
-  return (
-    <div>Demo: Manage Config</div>
-  )
-}
--- site/main/src/components/Demo/ManageData.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ManageData(){
-  return (
-    <div>Demo: Manage Data</div>
-  )
-}
