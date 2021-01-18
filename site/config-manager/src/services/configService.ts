6221f07bfca8498de4298e751ca5757208676b11
Jonathan Pritchard <jon@node28.com>
2021-01-18 20:57:27 +0000

    Tweaked service api

--- site/config-manager/src/services/configService.ts
+++ site/config-manager/src/services/configService.ts
@@ -1,28 +1,40 @@
-import { GetResult, ListResult, SaveResult, Service } from "@graphter/core";
+import { GetResult, PathSegment, Service } from "@graphter/core";
 
 import pageConfig from '../models/page'
 
+const configs: GetResult[] = [
+  {
+    item: {
+      id: 'page',
+      name: 'Page'
+    },
+    version: 1
+  },
+  {
+    item: {
+      id: 'author',
+      name: 'Author'
+    },
+    version: 2
+  }
+]
+
+
 export const configService: Service =  {
-  list: (modelId: string, skip: number, take: number) => {
+  list: (skip: number, take: number) => {
     return Promise.resolve({
-      items: [
-        pageConfig
-      ],
+      items: configs.map(config => config.item),
       count: 1,
       skip: 0,
       take: 10
     })
   },
-  get: (modelId: string, instanceId: string | number) => {
-    if(instanceId !== 'page') return Promise.resolve({
+  get: async (id: string | number) => {
+    return Promise.resolve(configs.find(config => config.item.id === id) || {
       item: null
     })
-    return Promise.resolve({
-      item: pageConfig,
-      version: 1
-    })
   },
-  save: (modelId: string, instance: any) => {
+  save: (id: string | number, data: any) => {
     throw new Error('Cannot save config yet')
   }
 }
