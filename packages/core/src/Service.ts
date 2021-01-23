3edaaf1ac9e20cd286d43b870e6bf45a6f09a1aa
Jonathan Pritchard <jon@node28.com>
2021-01-23 08:46:20 +0000

    List skip and take values are optional

--- packages/core/src/Service.ts
+++ packages/core/src/Service.ts
@@ -4,7 +4,7 @@ import {SaveResult} from "./SaveResult";
 import { PathSegment } from "./PathSegment";
 
 export interface Service {
-  list: (skip: number, take: number) => Promise<ListResult>,
+  list: (skip?: number, take?: number) => Promise<ListResult>,
   get: (id: string | number) => Promise<GetResult>,
   save: (id: string | number, data: any) => Promise<SaveResult>
 }
