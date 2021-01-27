00e6f6fb5a83ccd13f5035c1e39fdc52d6c7d4a8
Jonathan Pritchard <jon@node28.com>
2021-01-27 07:30:56 +0000

    Uses the new setupNodeRenderer.tsx wrapper

--- site/config-manager/src/node-renderers/id/IdNodeRenderer.tsx
+++ site/config-manager/src/node-renderers/id/IdNodeRenderer.tsx
@@ -1,22 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { ComponentType, useEffect, useState } from "react";
 import { NodeRendererProps } from "@graphter/core";
 import { createDefault, useNodeData, useNodeValidation } from "@graphter/renderer-react";
 import s from './IdNodeRenderer.module.css'
 import { nanoid } from 'nanoid'
+import { setupNodeRenderer } from "@graphter/renderer-react";
 
 const filterRegExp = /[^a-z0-9-]/
 const filterRegExpGlobal = /[^a-z0-9-]/g
 
-function IdNodeRenderer(
+const IdNodeRenderer: ComponentType<NodeRendererProps> = setupNodeRenderer((
   {
     config,
     originalNodeData,
     committed = true,
     path
   }: NodeRendererProps
-){
+) => {
   const isNew = typeof originalNodeData === 'undefined'
-  if(isNew) originalNodeData = createDefault(config, nanoid)
+  if(isNew) originalNodeData = createDefault(config, '')
   const [ touched, setTouched ] = useState(false)
   const [ nodeData, setNodeData ] = useNodeData(path, config, originalNodeData, committed)
   const validationResults = useNodeValidation(path)
@@ -55,6 +56,6 @@ function IdNodeRenderer(
       ))}
     </>
   )
-}
+})
 
 export default IdNodeRenderer
