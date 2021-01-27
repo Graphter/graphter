828510b11222c73edaa7da92341acff2fda82da6
Jonathan Pritchard <jon@node28.com>
2021-01-27 07:38:02 +0000

    Output modules

--- packages/renderer-react/src/index.ts
+++ packages/renderer-react/src/index.ts
@@ -4,12 +4,14 @@ import NodeEditRenderer from "./edit-renderer/NodeEditRenderer"
 import ValidationSummary from './edit-renderer/ValidationSummary'
 import NodeDataProvider from "./node-data-provider";
 import ServiceProvider from './service-provider'
+import ConfigProvider from './config-provider'
 
 export {
   ListRenderer,
   EditRenderer,
   NodeEditRenderer,
   ServiceProvider,
+  ConfigProvider,
   ValidationSummary,
   NodeDataProvider,
 }
@@ -23,3 +25,5 @@ export * from './util/path'
 export * from './node-validation-provider'
 export * from './node-data-provider'
 export * from './service-provider'
+export * from './config-provider'
+export * from './setup-node-renderer'
