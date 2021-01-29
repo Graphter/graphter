f27d1fba9844550ebb46aa10946cba11da8ab27d
Jonathan Pritchard <jon@node28.com>
2021-01-29 08:12:51 +0000

    Fixed manual mock

--- packages/renderer-component-library-react/__mocks__/@graphter/renderer-react.tsx
+++ packages/renderer-component-library-react/__mocks__/@graphter/renderer-react.tsx
@@ -21,7 +21,7 @@ export const useNodeValidation = jest.fn().mockResolvedValue([])
 export const nodeRendererStore = {
   get: jest.fn((nodeType: string) => ({
     type: 'string',
-    renderer: ({originalNodeData}: any) => {
+    Renderer: ({originalNodeData}: any) => {
       const [ value, setValue ] = useState(originalNodeData || '')
       return (
         <input value={value} onChange={e => setValue(e.target.value)} data-testid='string-renderer' />
