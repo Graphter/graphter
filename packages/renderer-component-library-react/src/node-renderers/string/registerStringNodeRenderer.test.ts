2aa57bad028e92576353ae6a0158eb9e2773bc19
Jonathan Pritchard <jon@node28.com>
2021-01-29 07:29:37 +0000

    Fixed and improved the registerStringNodeRenderer.test.ts

--- packages/renderer-component-library-react/src/node-renderers/string/registerStringNodeRenderer.test.ts
+++ packages/renderer-component-library-react/src/node-renderers/string/registerStringNodeRenderer.test.ts
@@ -3,6 +3,7 @@ import StringNodeRenderer, { registerStringNodeRenderer } from "../string";
 describe('registerStringNodeRenderer()', () => {
   it('should return the correct registration', async () => {
     const result = registerStringNodeRenderer()
+    expect(result.Renderer).toBe(StringNodeRenderer)
     expect(result).toMatchSnapshot()
   })
   it('should override the type if supplied in options', async () => {
