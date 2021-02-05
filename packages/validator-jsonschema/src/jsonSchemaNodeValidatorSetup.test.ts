12af6fe8e61cc9f7562f2da8d470079d381d97d0
Jonathan Pritchard <jon@node28.com>
2021-02-05 08:04:50 +0000

    Fixed up AJV tests

--- packages/validator-jsonschema/src/jsonSchemaNodeValidatorSetup.test.ts
+++ packages/validator-jsonschema/src/jsonSchemaNodeValidatorSetup.test.ts
@@ -4,8 +4,14 @@ import generateErrorMessage from "./generateErrorMessage";
 import jsonSchemaNodeValidatorSetup from './jsonSchemaNodeValidatorSetup'
 import { ValidationExecutionStage } from "@graphter/core";
 import ajvKeywords from "ajv-keywords";
+import Ajv from "ajv";
 
-jest.mock('ajv')
+jest.mock('ajv', () => {
+  const MockAjv = {
+    compile: jest.fn()
+  }
+  return jest.fn(() => MockAjv)
+})
 jest.mock('ajv-keywords')
 jest.mock('./generateErrorMessage')
 
@@ -13,6 +19,66 @@ const AJVMock = AJV as jest.Mocked<any>
 const generateErrorMessageMock = generateErrorMessage as jest.Mock<any>
 
 describe('jsonSchemaNodeValidatorSetup', () => {
+  let ajvMock: jest.Mocked<any>
+  beforeEach(() => {
+    ajvMock =  new Ajv();
+  })
+
+  it('should compile the schema to a validator function ', async () => {
+    ajvMock.compile.mockReturnValueOnce(() => true)
+    await jsonSchemaNodeValidatorSetup({ schema: { some: 'schema' } })
+    expect(ajvKeywords).toHaveBeenCalledWith(ajvMock)
+  })
+
+  it('should use ajv keywords library', async () => {
+    ajvMock.compile.mockReturnValueOnce(() => true)
+    await jsonSchemaNodeValidatorSetup({ schema: { some: 'schema' } })
+    expect(ajvMock.compile).toHaveBeenCalledWith({ some: 'schema' })
+  })
+
+  it('should use sensible defaults', async () => {
+    ajvMock.compile.mockReturnValueOnce(() => false)
+    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
+    await execute(ValidationExecutionStage.CHANGE,
+      { id: 'name', type: 'string' },
+      ['page'],
+      'some-data')
+    expect(generateErrorMessageMock).toHaveBeenCalled()
+    expect(generateErrorMessageMock.mock.calls[0][1]).toMatchSnapshot()
+  })
+
+  it('should execute the validator function', async () => {
+    const validatorMock = jest.fn().mockReturnValueOnce(true)
+    ajvMock.compile.mockReturnValueOnce(validatorMock)
+    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
+    await execute(ValidationExecutionStage.CHANGE,
+      { id: 'name', type: 'string' },
+      ['page'],
+      'some-data')
+    expect(validatorMock).toHaveBeenCalledWith('some-data')
+  })
+
+  it('should return the correct result when data is valid', async () => {
+    const validatorMock = jest.fn().mockReturnValueOnce(true)
+    ajvMock.compile.mockReturnValueOnce(validatorMock)
+    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
+    const result = await execute(ValidationExecutionStage.CHANGE,
+      { id: 'name', type: 'string' },
+      ['page'],
+      'some-data')
+    expect(result).toMatchSnapshot()
+  })
+
+  it('should return the correct result when data is not valid', async () => {
+    const validatorMock = jest.fn().mockReturnValueOnce(false)
+    ajvMock.compile.mockReturnValueOnce(validatorMock)
+    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
+    const result = await execute(ValidationExecutionStage.CHANGE,
+      { id: 'name', type: 'string' },
+      ['page'],
+      'some-data')
+    expect(result).toMatchSnapshot()
+  })
 
   it('should error when no options are provided', async () => {
     // @ts-ignore
@@ -26,66 +92,6 @@ describe('jsonSchemaNodeValidatorSetup', () => {
       .toThrow('Schema option is required by JsonSchemaValidator')
   })
 
-  it('should use sensible defaults', async () => {
-    AJVMock.mockImplementation(() => ({
-      compile: () => () => false
-    }))
-    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
-    execute(ValidationExecutionStage.CHANGE,
-      { id: 'name', type: 'string' },
-      ['page'],
-      'some-data')
-    expect(generateErrorMessageMock).toHaveBeenCalled()
-    expect(generateErrorMessageMock.mock.calls[0][1]).toMatchSnapshot()
-  })
-
-  it('should compile the schema to a validator function ', async () => {
-    const compileMock = jest.fn().mockReturnValue(() => true)
-    AJVMock.mockImplementation(() => ({
-      compile: compileMock
-    }))
-    await jsonSchemaNodeValidatorSetup({ schema: { some: 'schema' } })
-    expect(compileMock).toHaveBeenCalledWith({ some: 'schema' })
-  })
-
-  it('should execute the validator function', async () => {
-    const validatorMock = jest.fn().mockReturnValue(true)
-    AJVMock.mockImplementation(() => ({
-      compile: () => validatorMock
-    }))
-    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
-    execute(ValidationExecutionStage.CHANGE,
-      { id: 'name', type: 'string' },
-      ['page'],
-      'some-data')
-    expect(validatorMock).toHaveBeenCalledWith('some-data')
-  })
-
-  it('should return the correct result when data is valid', async () => {
-    const validatorMock = jest.fn().mockReturnValue(true)
-    AJVMock.mockImplementation(() => ({
-      compile: () => validatorMock
-    }))
-    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
-    const result = execute(ValidationExecutionStage.CHANGE,
-      { id: 'name', type: 'string' },
-      ['page'],
-      'some-data')
-    expect(result).toMatchSnapshot()
-  })
-
-  it('should return the correct result when did is not valid', async () => {
-    const validatorMock = jest.fn().mockReturnValue(false)
-    AJVMock.mockImplementation(() => ({
-      compile: () => validatorMock
-    }))
-    const execute = await jsonSchemaNodeValidatorSetup({ schema: {} })
-    const result = execute(ValidationExecutionStage.CHANGE,
-      { id: 'name', type: 'string' },
-      ['page'],
-      'some-data')
-    expect(result).toMatchSnapshot()
-  })
 
 })
 
