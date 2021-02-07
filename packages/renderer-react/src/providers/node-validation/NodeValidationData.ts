8e6c2387d940754b3a821e0f258ecdfd4e385c6e
Jonathan Pritchard <jon@node28.com>
2021-02-07 11:18:46 +0000

    Removed config from NodeValidationData interface because it was awkward when initialising state on the fly. Hope this doesn't come back to bite me on the arse 🤞

--- packages/renderer-react/src/providers/node-validation/NodeValidationData.ts
+++ packages/renderer-react/src/providers/node-validation/NodeValidationData.ts
@@ -1,8 +1,7 @@
-import { NodeConfig, PathSegment, ValidationResult } from "@graphter/core";
+import { PathSegment, ValidationResult } from "@graphter/core";
 
 export interface NodeValidationData {
   path: Array<PathSegment>
-  config?: NodeConfig
   value?: any
   results: Array<ValidationResult>
 }
