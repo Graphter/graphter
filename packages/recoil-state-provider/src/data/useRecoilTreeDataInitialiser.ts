44514cd05dae606faa592440109024f248f41877
Jonathan Pritchard <jon@node28.com>
2021-02-09 07:59:24 +0000

    Ammended the recoil tree initialiser hook to now init from renderer registrations

--- packages/recoil-state-provider/src/data/useRecoilTreeDataInitialiser.ts
+++ packages/recoil-state-provider/src/data/useRecoilTreeDataInitialiser.ts
@@ -1,9 +1,15 @@
 import { propDataStore } from "../store/propDataStore";
 import { TreeDataInitialiserHook } from "@graphter/renderer-react";
+import { nodeRendererStore } from "@graphter/renderer-react";
 
 export const useRecoilTreeDataInitialiser: TreeDataInitialiserHook = () => {
-  return (path, originalTreeData) => {
-    if(typeof originalTreeData === 'undefined') return
-    propDataStore.init(path, originalTreeData)
+  return (config, path, originalTreeData) => {
+    const rendererRegistration = nodeRendererStore.get(config.type)
+    if(!rendererRegistration) throw new Error(`Unable to find renderer for node type '${config.type}' at ${path.join('/')}`)
+    rendererRegistration.initialiseData ?
+      rendererRegistration.initialiseData(config, path, originalTreeData, (path, originalData) => {
+        propDataStore.set(path, true, originalData)
+      }) :
+      propDataStore.set(path, true, originalTreeData)
   }
 }
