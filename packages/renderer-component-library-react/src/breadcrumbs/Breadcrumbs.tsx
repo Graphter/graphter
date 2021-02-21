7515712ba419bbef3d6d8abb0ebc676b303776bb
Jonathan Pritchard <jon@node28.com>
2021-02-21 17:17:26 +0000

    Added breadcrumb dividers in

--- packages/renderer-component-library-react/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/renderer-component-library-react/src/breadcrumbs/Breadcrumbs.tsx
@@ -27,8 +27,7 @@ interface CrumbData {
 }
 
 const guessingNames = [ 'name', 'title', 'headline', 'label', 'id' ]
-const NoMatch = '8024a530-29be-4c9f-b61f-6a4525b18e9a'
-const DividerSvg = () => <svg className='w-4 fill-current text-gray-200 mr-2' xmlns="http://www.w3.org/2000/svg"
+const DividerSvg = () => <svg className='inline-block w-4 fill-current text-gray-200 mr-2' xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 20 20" fill="currentColor">
   <path fillRule="evenodd"
         d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
@@ -61,19 +60,24 @@ const Breadcrumbs = ({config, globalPath, ItemRenderer, originalTreeData}: Bread
 
   return (
     <div>
-      {crumbsData.map(crumbData => (
-        <ItemRenderer key={crumbData.path.join('/')} path={crumbData.path}>
-          {crumbData.display.path ? (
-            <DynamicCrumb
-              displayPath={crumbData.display.path}
-              displayLabel={crumbData.display.label}
-            />
-          ) : (
-            crumbData.display.label
-          )}
-        </ItemRenderer>
-      )
-    )}
+      {crumbsData.map((crumbData, i) => (
+          <>
+            <ItemRenderer key={crumbData.path.join('/')} path={crumbData.path}>
+              {crumbData.display.path ? (
+                <DynamicCrumb
+                  displayPath={crumbData.display.path}
+                  displayLabel={crumbData.display.label}
+                />
+              ) : (
+                crumbData.display.label
+              )}
+            </ItemRenderer>
+            {i < crumbsData.length - 1 && (
+              <DividerSvg/>
+            )}
+          </>
+        )
+      )}
     </div>
   )
 }
