5ac5699364f4237adc776f4592c854d577c09e98
Jonathan Pritchard <jon@node28.com>
2021-04-05 16:08:22 +0100

    Removed unused value init util

--- packages/renderer-react/src/node-renderer-utils/createValueInitialiser.ts
+++
@@ -1,8 +0,0 @@
-import { pathUtils } from "../util/path";
-import { createDefault } from "../util/node";
-import { InitialiseNodeDataFn } from "@graphter/core";
-
-export const createValueInitialiser = (fallbackValue: any): InitialiseNodeDataFn =>
-  (config, path, initialise, originalTreeData) => {
-    initialise(path, pathUtils.getValue(originalTreeData, path.slice(2), createDefault(config, fallbackValue)))
-  }
