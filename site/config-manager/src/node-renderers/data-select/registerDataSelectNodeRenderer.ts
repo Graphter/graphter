e7363c03c98499a326304cb8035c53a44fe26e59
Jonathan Pritchard <jon@node28.com>
2021-04-18 15:06:02 +0100

    Default to empty to enable the automatic selection of the first item from the list

--- site/config-manager/src/node-renderers/data-select/registerDataSelectNodeRenderer.ts
+++ site/config-manager/src/node-renderers/data-select/registerDataSelectNodeRenderer.ts
@@ -11,7 +11,7 @@ export function registerDataSelectNodeRenderer(options?: DataSelectNodeRendererO
     type: options?.type || 'data-select',
     name: 'Data Select',
     description: 'Manage a selection from a set of options supplied by a data source',
-    createFallbackDefaultValue: () => Promise.resolve('text'),
+    createFallbackDefaultValue: () => Promise.resolve(''),
     Renderer: DataSelectNodeRenderer,
     options,
     optionsConfig: dataSelectOptionsConfig
