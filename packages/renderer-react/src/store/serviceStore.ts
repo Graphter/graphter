9bfadf70a4f0309ebe7e28de36b22e83e84649bb
Jonathan Pritchard <jon@node28.com>
2021-04-18 15:04:28 +0100

    Using Map() instead of an object and adding a couple of functions to expose the list of services

--- packages/renderer-react/src/store/serviceStore.ts
+++ packages/renderer-react/src/store/serviceStore.ts
@@ -1,20 +1,31 @@
 import { Service } from "@graphter/core";
 
-const serviceMap: { [key: string]: Service } = {};
+const serviceMap: Map<string, Service> = new Map();
 
 const register = (serviceId: string, service: Service) => {
-  serviceMap[serviceId] = service
+  serviceMap.set(serviceId, service)
 }
+
+const has = (serviceId: string): boolean => {
+  return serviceMap.has(serviceId)
+}
+
 const get = (serviceId: string): Service => {
-  let registration = serviceMap[serviceId]
+  let registration = serviceMap.get(serviceId)
   if(!registration) {
-    console.warn(`No '${serviceId}' service has been registered. Moving on...`)
+    throw new Error(`No service with ID '${serviceId}' has been registered`)
   }
   return registration
 }
+const getAll = (): Array<Service> => {
+  return Array.from(serviceMap.values())
+}
 export const serviceStore = {
   register,
+  has,
   get,
+  getAll,
+  getMap: () => serviceMap
 }
 
 export default serviceStore
