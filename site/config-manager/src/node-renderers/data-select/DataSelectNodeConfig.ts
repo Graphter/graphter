5be31f128c3034f2ce3469ff9f2f49cddf7f616c
Jonathan Pritchard <jon@node28.com>
2021-04-18 14:57:06 +0100

    Fixed DataSelectNodeConfig definition

--- site/config-manager/src/node-renderers/data-select/DataSelectNodeConfig.ts
+++ site/config-manager/src/node-renderers/data-select/DataSelectNodeConfig.ts
@@ -1,7 +1,11 @@
 import { NodeConfig, PathSegment } from "@graphter/core";
 
 export interface DataSelectNodeConfig extends NodeConfig {
-  children: [ NodeConfig, ...[NodeConfig] ]
+  children: [ ]
+  options: DataSelectNodeConfigOptions
+}
+
+export interface DataSelectNodeConfigOptions {
   serviceId: string
   keyPath: Array<PathSegment>
   valuePath: Array<PathSegment>
