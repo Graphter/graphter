aaeda6aa439b603dc8c75e749977f155fd211234
Jonathan Pritchard <jon@node28.com>
2021-05-16 10:49:40 +0100

    Added some fallback logic in case I don't ever get init working properly

--- packages/recoil-state-provider/src/data/useRecoilNodeData.ts
+++ packages/recoil-state-provider/src/data/useRecoilNodeData.ts
@@ -3,13 +3,18 @@ import { NodeDataHook } from "@graphter/renderer-react";
 import { rendererInternalDataStore } from "../store/rendererInternalDataStore";
 import { NodeConfig, PathSegment } from "@graphter/core";
 
-/**
- * @param path
- */
 export const useRecoilNodeData: NodeDataHook = <D>(path: Array<PathSegment>, config: NodeConfig) => {
-
+  /**
+   * Having to use this code indicates init logic isn't working well.
+   * Data should be initialised properly before a node ever tries to access it.
+   */
+    // if(!rendererInternalDataStore.has(path, config)){
+    //   const nodeRendererReg = nodeRendererStore.get(config.type)
+    //   const defaultInternalData = nodeRendererReg.createFallbackDefaultValue?.(config, path) || null
+    //   rendererInternalDataStore.set(path, config, defaultInternalData)
+    // }
   const nodeInternalState = rendererInternalDataStore.get<D>(path, config)
-  if(!nodeInternalState) throw new Error(`Should have internal data state at '${path.join('/')}' ${config.id}[type=${config.type}] by now`)
+  if (!nodeInternalState) throw new Error(`Should have internal data state at '${path.join('/')}' ${config.id}[type=${config.type}] by now`)
 
   return useRecoilState<D>(nodeInternalState)
 }
