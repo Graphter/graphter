b1c0d43c77734d59f1094001f3d8fbc91c7d34e2
Jonathan Pritchard <jon@node28.com>
2021-05-16 11:37:28 +0100

    Better naming

--- packages/recoil-state-provider/src/data/useRecoilDownstreamNodeConfigs.ts
+++ packages/recoil-state-provider/src/data/useRecoilDownstreamNodeConfigs.ts
@@ -1,4 +1,4 @@
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { nodeConfigSetsStore } from "../store/nodeConfigSetsStore";
 import { NodeConfig } from "@graphter/core";
 import { useRecoilTreeDataInitialiser } from "./useRecoilTreeDataInitialiser";
@@ -8,6 +8,11 @@ import { pathUtils } from "@graphter/renderer-react";
 
 const configToString = (config: NodeConfig) => `${config.id}[type=${config.type}]`
 
+/**
+ * TODO: Rename useRecoilNodeChildConfigs or something...
+ * @param path
+ * @param config
+ */
 export const useRecoilDownstreamNodeConfigs: DownstreamNodeConfigsHook = (
   path,
   config
@@ -15,17 +20,17 @@ export const useRecoilDownstreamNodeConfigs: DownstreamNodeConfigsHook = (
   const nodeConfigSetsState = nodeConfigSetsStore.get(path)
   if (!nodeConfigSetsState) throw new Error(`Missing configs state at '${path.join('/')}'`)
 
-  const [ nodeConfigSets, setNodeConfigs ] = useRecoilState(nodeConfigSetsState)
+  const nodeConfigSets = useRecoilValue(nodeConfigSetsState)
   const configs = nodeConfigSets.configSets.get(nodeConfigSets.activeConfigsKey)
   if(!configs) throw new Error(`No active configs found at '${path.join('/')}'`)
 
   const treeDataInitialiser = useRecoilTreeDataInitialiser()
   const initialiseBranch = useRecoilTreeDataCallback<Array<NodeConfig>>(async (treeData, newConfig) => {
-    const topNodeConfig = configs[0]
-    const replacingTopConfig = topNodeConfig.id === newConfig.id && topNodeConfig.type === topNodeConfig.type
+    const firstNodeConfig = configs[0]
+    const replacingFirstConfig = firstNodeConfig.id === newConfig.id && firstNodeConfig.type === firstNodeConfig.type
     // Delete the section of the tree we're about to (re)initialise so that any new branches have default data
     const obscuredTreeData = pathUtils.deleteAtGlobalPath(treeData, path)
-    treeDataInitialiser(replacingTopConfig ? newConfig : topNodeConfig, path, obscuredTreeData)
+    treeDataInitialiser(replacingFirstConfig ? newConfig : firstNodeConfig, path, obscuredTreeData)
   }, path.slice(0, 2))
 
   const currentConfigIndex = configs.findIndex(eachConfig => eachConfig.id === config.id && eachConfig)
