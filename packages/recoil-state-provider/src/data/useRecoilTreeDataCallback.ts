40e609697ce6b4ea536a282cea297fd570e0f3c6
Jonathan Pritchard <jon@node28.com>
2021-06-05 12:16:24 +0100

    Await the callback in useRecoilTreeDataCallback.ts

--- packages/recoil-state-provider/src/data/useRecoilTreeDataCallback.ts
+++ packages/recoil-state-provider/src/data/useRecoilTreeDataCallback.ts
@@ -6,7 +6,7 @@ export const useRecoilTreeDataCallback: TreeDataCallbackHook = (fn, path, depth)
 
   const cb = useRecoilCallback(({snapshot}) => async (...args: any) => {
     const treeData = await snapshot.getPromise(branchDataStore.get(path, depth))
-    fn(treeData, ...args)
+    await fn(treeData, ...args)
   }, [ path ])
 
   return cb
