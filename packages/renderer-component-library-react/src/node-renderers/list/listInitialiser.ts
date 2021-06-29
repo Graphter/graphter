0f20a3d4ec6d829e7a6e39bc7382029bace43ef2
Jonathan Pritchard <jon@node28.com>
2021-06-29 08:57:22 +0100

    Var rename

--- packages/renderer-component-library-react/src/node-renderers/list/listInitialiser.ts
+++ packages/renderer-component-library-react/src/node-renderers/list/listInitialiser.ts
@@ -10,16 +10,16 @@ export const listInitialiser: NodeDataInitialiserFn = async (
   path
 ) => {
   if (!isListConfig(config)) throw new Error('ListNodeRenderer received invalid config')
-  let originalNodeData = await getBranchData(path)
-  if (typeof originalNodeData === 'undefined') {
+  let extractedExternalData = await getBranchData(path)
+  if (typeof extractedExternalData === 'undefined') {
     const rendererReg = nodeRendererStore.get(config.type)
-    originalNodeData = await createDefault(config, rendererReg.createFallbackDefaultValue ? rendererReg.createFallbackDefaultValue(config, path, getBranchData) : null)
+    extractedExternalData = await createDefault(config, rendererReg.createFallbackDefaultValue ? rendererReg.createFallbackDefaultValue(config, path, getBranchData) : null)
   }
-  if (!Array.isArray(originalNodeData)) throw new Error(`List initialiser received unexpected original data type: ${JSON.stringify(originalNodeData)}`)
+  if (!Array.isArray(extractedExternalData)) throw new Error(`List initialiser received unexpected original data type: ${JSON.stringify(extractedExternalData)}`)
   const nodeMeta = {
     path,
     config,
-    internalData: originalNodeData.map(item => ({
+    internalData: extractedExternalData.map(item => ({
       item,
       key: nanoid(),
       committed: true,
@@ -30,7 +30,7 @@ export const listInitialiser: NodeDataInitialiserFn = async (
   const childRendererReg = nodeRendererStore.get(childConfig.type)
 
   if (!childRendererReg) throw new Error(`Couldn't find a renderer registration for type '${childConfig.type}'`)
-  const childNodeMetas = (await Promise.all(originalNodeData.map((childData, i) => {
+  const childNodeMetas = (await Promise.all(extractedExternalData.map((childData, i) => {
     const childPath = [ ...path, i ]
     if (childRendererReg?.initialiser) return childRendererReg?.initialiser(getBranchData, childConfig, childPath)
     return Promise.resolve([
